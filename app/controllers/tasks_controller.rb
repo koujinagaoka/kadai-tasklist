6c9f9c9cc326b89b0e948d83bc0abdcd9df209fb
kouji nagaoka <ootomowashin629@gmail.com>
2020-08-14 08:54:12 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,12 +24,26 @@ class TasksController < ApplicationController
     end
     
     def edit
+        @task = Task.find(params[:id])
     end
     
     def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            flash[:success] = 'TASK は正常に更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'TASK は更新されませんでした'
+            render :edit
+        end
     end
     
     def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'TASK は正常に削除されました'
+        redirect_to tasks_url
     end
     
 end
