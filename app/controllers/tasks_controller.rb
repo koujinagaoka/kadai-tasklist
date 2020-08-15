358581f3e7969244350a843532213ff18c42804b
kouji nagaoka <ootomowashin629@gmail.com>
2020-08-15 02:10:08 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        
+        @task = Task.new(task_params)
         if @task.save
             flash[:success] = 'TASK が正常に投稿されました'
             redirect_to @task
